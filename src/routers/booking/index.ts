f2b23c358b6e137ce43e404761fbd515818eb86e
deepg7 <deepgandhi151@gmail.com>
2023-01-30 20:01:36 +0530

    corrected booking patch and veh fetch for station

--- src/routers/booking/index.ts
+++ src/routers/booking/index.ts
@@ -5,17 +5,44 @@ import Vehicle from "../../models/vehicle";
 
 const router = Router();
 
+const ACTIVE = "active";
+const INACTIVE = "inactive";
+
 router.post("/:uid/:vid", async (req: Request, res: Response) => {
   try {
     const { uid, vid } = req.params;
     const user = await User.findByPk(uid);
     const vehicle = await Vehicle.findByPk(vid);
-    if (!vehicle || !user) return res.send("bye");
+    if (!vehicle || !user) throw new Error("err");
     const booking = await Booking.create({
       VehicleId: Number(vid),
       UserId: Number(uid),
-      status: "active",
+      status: ACTIVE,
     });
+    return res.status(201).send(booking);
+  } catch (e) {
+    return res.send(e);
+  }
+});
+
+//search with id and active
+//if not found then return error
+//if successful update vehicle station id to new stop
+//toDO ADD A TRANSACTION HERE
+router.patch("/:id/:sid", async (req: Request, res: Response) => {
+  try {
+    const { id, sid } = req.params;
+    if (!id || !sid) throw new Error("empty");
+    const booking = await Booking.update(
+      { status: INACTIVE },
+      { where: { id } }
+    );
+    const vehicleID = (await Booking.findByPk(id))?.VehicleId;
+    if (!vehicleID) throw new Error("err");
+    const vehicle = await Vehicle.update(
+      { stationID: Number(sid) },
+      { where: { id: vehicleID } }
+    );
     return res.status(201).send(booking);
   } catch (e) {
     return res.send(e);
--- src/routers/vehicle/index.ts
+++ src/routers/vehicle/index.ts
@@ -1,13 +1,34 @@
 import { Request, Response, Router } from "express";
+import { Op } from "sequelize";
+import Booking from "../../models/booking";
 import Vehicle, { VehicleInput } from "../../models/vehicle";
 
 const router = Router();
 
+//CREATE VEHICLE
 router.post("/", async (req: Request, res: Response) => {
   try {
     const payload = req.body.payload as VehicleInput;
     const vehicle = await Vehicle.create(payload);
     return res.status(201).send(vehicle);
+  } catch (e) {
+    return res.send(e);
+  }
+});
+
+//GET VEHICLE FOR A PARTICULAR STATION
+router.get("/:sid", async (req: Request, res: Response) => {
+  try {
+    const { sid } = req.params;
+    const vehicles = await Vehicle.findAll({ where: { stationID: sid } });
+    // return res.send(vehicles);
+    const vids = vehicles.map((v) => v.id);
+    const bookings = await Booking.findAll({
+      where: { VehicleId: { [Op.in]: vids }, status: "active" },
+    });
+    const bookedVIDs = bookings.map((b) => b.VehicleId);
+    const avlblV = vehicles.filter((v) => !bookedVIDs.includes(v.id));
+    return res.send(avlblV);
   } catch (e) {
     return res.send(e);
   }
